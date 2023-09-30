e34fc57cc35b60a99fccfe2912798042eeb56ce4
Tom Villegas <tvillega@dcc.uchile.cl>
2023-09-30 23:02:01 +0000

    docs(countdown): add docstring

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import time
 
 def countdown():
+    """ Counts from 4 to 0
+
+    This functions is a countdown that starts at 5 and ends at 0
+    """
 
     for i in range(0,5): print(4-i) ; time.sleep(0.5)
 
