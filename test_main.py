e4d5a4e02a8841b1439c8949243887c9e3783ea4
Tom Villegas <tvillega@dcc.uchile.cl>
2023-09-30 23:15:30 +0000

    test(countdown): function return

---
+++ test_main.py
@@ -0,0 +1,12 @@
+import unittest
+
+import main
+
+class TestMyProgram(unittest.TestCase):
+
+    def test_countdown(self):
+        self.assertEqual(self.countdown, -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
